1f2989896f4ae917622ca9916a26b7bee02a5f90
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2017-05-08 19:06:31 +0200

    Step 1: define a player with its test class and a working test which test first value

---
+++ src/test/java/fr/chaffotm/coding/dojo/fizzbuzz/PlayerTest.java
@@ -0,0 +1,38 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.chaffotm.coding.dojo.fizzbuzz;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Matthieu CHAFFOTTE
+ */
+public class PlayerTest {
+
+    @Test
+    public void say_should_answer_one_with_one() throws Exception {
+        //Given
+        Player player = new Player();
+
+        //When
+        int response = player.say(1);
+
+        //Then
+        assertThat(response).isEqualTo(1);
+    }
+
+}
